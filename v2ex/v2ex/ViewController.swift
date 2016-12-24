916ba6b9fa9a47ba00ac16a611fa3a624e28a394
zzgo <zzgocc@gmail.com>
2016-12-24 15:05:00 +0800

    +add pod

--- v2ex/v2ex/ViewController.swift
+++ v2ex/v2ex/ViewController.swift
@@ -7,6 +7,35 @@
 //
 
 import UIKit
+import RAMAnimatedTabBarController
+
+import RAMAnimatedTabBarController
+
+class RAMBounceAnimation : RAMItemAnimation {
+    
+    override func playAnimation(_ icon: UIImageView, textLabel: UILabel) {
+        playBounceAnimation(icon)
+        textLabel.textColor = textSelectedColor
+    }
+    
+    override func deselectAnimation(_ icon: UIImageView, textLabel: UILabel, defaultTextColor: UIColor, defaultIconColor: UIColor) {
+        textLabel.textColor = defaultTextColor
+    }
+    
+    override func selectedState(_ icon: UIImageView, textLabel: UILabel) {
+        textLabel.textColor = textSelectedColor
+    }
+    
+    func playBounceAnimation(_ icon : UIImageView) {
+        
+        let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+        bounceAnimation.values = [1.0 ,1.4, 0.9, 1.15, 0.95, 1.02, 1.0]
+        bounceAnimation.duration = TimeInterval(duration)
+        bounceAnimation.calculationMode = kCAAnimationCubic
+        
+        icon.layer.add(bounceAnimation, forKey: "bounceAnimation")
+    }
+}
 
 class ViewController: UIViewController {
 
